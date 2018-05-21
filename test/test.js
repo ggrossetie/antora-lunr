a19e3a07cedbc385a78028041188d8c88bc4b9c9
Guillaume Grossetie <ggrossetie@gmail.com>
2018-05-21 12:24:49 +0200

    Index titles

--- test/test.js
+++ test/test.js
@@ -65,4 +65,42 @@ describe('Generate index', function () {
     expect(index.index.search('*foo*'), '*foo* is present in contents').to.have.lengthOf(1)
     expect(index.index.search('foo*'), 'foo* is present in contents').to.have.lengthOf(1)
   })
+  it('should generate an document for each titles', function () {
+    const playbook = {
+      site: {
+        url: 'https://docs.antora.org'
+      }
+    }
+    const pages = [{
+      contents: Buffer.from(`<h1>Antora Documentation</h1>
+<p>The Static Site Generator for Tech Writers</p>
+<p>This site hosts the technical documentation for Antora</p>
+<h2 id="manage-docs-as-code">Manage docs as code</h2>
+<p>With Antora, you manage docs as code</p>
+<h3 id="where-to-begin">Where to begin</h3>
+<h4 id="navigation">Navigation</h4>
+<h5 id="link-types-syntax">Link Types & Syntax</h5>
+<h6 id="page-links">Page Links</h6>`),
+      src: {
+        component: 'hello',
+        version: '1.0',
+        stem: ''
+      },
+      pub: {
+        url: '/antora/1.0/'
+      }
+    }]
+    const index = generateIndex(playbook, pages)
+    const installPage = index.store['https://docs.antora.org/antora/1.0/'];
+    expect(installPage.text).to.equal('The Static Site Generator for Tech Writers This site hosts the technical documentation for Antora With Antora, you manage docs as code')
+    expect(installPage.component).to.equal('hello')
+    expect(installPage.version).to.equal('1.0')
+    expect(installPage.title).to.equal('Antora Documentation')
+    expect(index.index.search('1.0'), 'version is not indexed').to.be.empty()
+    expect(index.index.search('bar'), 'bar is not present').to.be.empty()
+    expect(index.index.search('where to begin'), 'Where to begin is indexed as a title').to.have.lengthOf(1)
+    expect(index.index.search('docs as code'), 'docs as code is indexed two times').to.have.lengthOf(2)
+    expect(index.index.search('technical'), 'technical is indexed').to.have.lengthOf(1)
+    expect(index.index.search('hello'), 'hello is indexed as component').to.have.lengthOf(1)
+  })
 })
