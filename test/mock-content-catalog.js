256339d6795f525e922dda91718c9e5e62d309bc
Daniel Mulholland <dan.mulholland@gmail.com>
2020-05-03 20:59:41 +0000

    Improve code style and reference to Antora docs for mock content catalog

--- test/mock-content-catalog.js
+++ test/mock-content-catalog.js
@@ -1,5 +1,13 @@
 'use strict'
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+/* This source code is taken frome the Antora test suite with minor modifications
+ *  see https://gitlab.com/antora/antora/-/blob/master/test/mock-content-catalog.js
+ *  Copyright © 2017-2020 by OpenDevise Inc. and the individual contributors to Antora. */
+
 const { posix: path } = require('path')
 
 const SPACE_RX = / /g
